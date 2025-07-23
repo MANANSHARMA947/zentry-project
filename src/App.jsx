704fc3f43ec9a37ffbcbc06da958b041939758e6
Manan <manan.sharma2006gg@gmail.com>
2025-07-24 01:00:27 +0530

    Zentry

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidder">
-      <Hero/>
+      <Hero />
     </div>
-  )
+  );
 };
 
 export default App;
