b8d78291526796b5469f6f59e1fc9b90022eb379
Manan <manan.sharma2006gg@gmail.com>
2025-07-28 22:38:57 +0530

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./About";
 const App = () => {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidder">
       <Hero />
+      {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
+      <About/>
     </div>
   );
 };
