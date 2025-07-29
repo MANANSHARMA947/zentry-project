73f9dc49b920af97edd99fc8a5f361601439409e
Manan <manan.sharma2006gg@gmail.com>
2025-07-29 23:13:22 +0530

    About

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,22 @@
+import { useGSAP } from "@gsap/react";
 import React from "react";
-
+import gsap from "gsap";
 const About = () => {
+
+  useGSAP(()=>{
+    const clipAnimation = gsap.timeline({
+      scrollTrigger:{
+        trigger:'#clip',
+        start:'center center',
+        end: '+=800 center',
+        scrub:0.5,
+        pin:true,
+        pinSpacing:true,
+
+      }
+    })
+    clipAnimation
+  })
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
@@ -16,7 +32,13 @@ const About = () => {
         </div>
       </div>
       <div className="h-dvh w-screen" id="clip">
-
+      <div className="mask-clip-path about-image">
+        <img
+        src="img/about.webp"
+        alt="Background"
+        className="absolute left-0 top-0 size-full object-cover"
+        />
+      </div>
       </div>
     </div>
   );
