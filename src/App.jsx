6535e947248e9790e8e232d9e163727bde7ed73d
Manan <manan.sharma2006gg@gmail.com>
2025-07-31 23:08:51 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 const App = () => {
   return (
-    <div className="relative min-h-screen w-screen overflow-x-hidder bg-zinc-600">
+    <div className="relative min-h-screen w-screen overflow-x-hidder">
       <Navbar />
       <Hero />
       {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
