2339dccda07928461d4c4607bab1a7eed1fc5be0
Manan <manan.sharma2006gg@gmail.com>
2025-07-31 22:26:13 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./components/Hero";
 import About from "./About";
+import Navbar from "./Navbar";
 const App = () => {
   return (
-    <div className="relative min-h-screen w-screen overflow-x-hidder">
+    <div className="relative min-h-screen w-screen overflow-x-hidder bg-zinc-600">
+      
       <Hero />
       {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
       <About/>
