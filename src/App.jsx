5d6b55ab9b38e86b6b5a51c91c56c9a8946470d7
Manan <manan.sharma2006gg@gmail.com>
2025-08-13 20:43:10 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidder">
@@ -9,6 +13,10 @@ const App = () => {
       <Hero />
       {/* <section className="z-0 min-h-screen bg-blue-500" /> */}
       <About />
+      <Features />
+      <Story />
+      <Contact/>
+      <Footer/>
     </div>
   );
 };
